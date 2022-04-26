4107075a896a6d84cf3972068174ccc85c869aa1
JJY <jj2915@nyu.edu>
2022-04-25 20:59:42 -0400

    update syntax error

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -15,6 +15,7 @@ router.post("/get-weather-icon", async (req, res) => {
         const {lat, lon} = req.body;
         const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${process.env.APIKEY}`;
         let iconUrl = "";
+        let iconDes = "";
         let response = {};
 
         try {
@@ -25,7 +26,7 @@ router.post("/get-weather-icon", async (req, res) => {
         
         if (response.data) {
             iconUrl = `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`;
-            iconDes = response.data.weather[0].description
+            iconDes = response.data.weather[0].description;
             req.user.iconUrl = iconUrl;
             req.user.iconDes = iconDes;
         }
@@ -38,28 +39,24 @@ router.post("/get-weather-icon", async (req, res) => {
 router.post("/edit-spending", async (req, res) => {
     if (!req.isAuthenticated) {
         res.json({error: "What are you thinking about?"});
+    } else if (req.body.notes.length >= 50 || isNaN(parseFloat(req.body.amount)) || !moment(req.body.date, dateFormat, true).isValid()) {
+        res.json({error: "Invalid information"});
     } else {
-        if (req.body.notes.length >= 50 || isNaN(parseFloat(req.body.amount)) || !moment(req.body.date, dateFormat, true).isValid()) {
-            res.json({error: "Invalid information"});
-        } else {
-            const filter = {user: req.user.id, name: req.body.name};
-            const cat = await Category.findOne(filter);
-            const dbSpendings = cat.spendings;
-            const obj = dbSpendings.find(s => s._id.toString() === req.body._id);
-            
-            if (obj) {
-                obj.date = req.body.date;
-                obj.amount = parseFloat(req.body.amount);
-                obj.notes = req.body.notes;
-                await cat.save();
-                res.json({msg: "success"});
-            } else {
-                res.json({error: "Invalid information"});
-            }
-        }
+        const filter = {user: req.user.id, name: req.body.name};
+        const cat = await Category.findOne(filter);
+        const dbSpendings = cat.spendings;
+        const obj = dbSpendings.find(s => s._id.toString() === req.body._id);
         
+        if (obj) {
+            obj.date = req.body.date;
+            obj.amount = parseFloat(req.body.amount);
+            obj.notes = req.body.notes;
+            await cat.save();
+            res.json({msg: "success"});
+        } else {
+            res.json({error: "Invalid information"});
+        }
     }
-    
 });
 
 
